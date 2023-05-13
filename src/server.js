f897bb86e28a82f2ac457a1f51774566d34bebd5
yangyaaaa <yangya.lackner@silentdays.at>
2023-05-13 20:54:40 +0200

    email sever

--- src/server.js
+++ src/server.js
@@ -13,10 +13,10 @@ console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
 
 const contactEmail = nodemailer.createTransport({
-  service: 'gmail',
+  service: 'qq',
   auth: {
-    user: "********@gmail.com",
-    pass: ""
+    user: "150203531@qq.com",
+    pass: "ham-fishsp0218"
   },
 });
 
@@ -35,7 +35,7 @@ router.post("/contact", (req, res) => {
   const phone = req.body.phone;
   const mail = {
     from: name,
-    to: "********@gmail.com",
+    to: "150203531@qq.com",
     subject: "Contact Form Submission - Portfolio",
     html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
